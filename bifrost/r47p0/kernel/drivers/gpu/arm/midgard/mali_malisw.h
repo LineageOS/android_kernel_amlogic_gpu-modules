5880d87f0c44e0a0a574433fed1e15abf04b9cd5
Vitaliy Tomin <highwaystar.ru@gmail.com>
2026-07-31 20:03:28 +0800

    Revert "bifrost: r47p0: avoid MIN/MAX redefinition against 5.15.202 minmax.h"
    This reverts commit 4c2f34070310c7b3e7ec03ff8d7a91d39a4da249.
    
    The #undef only silences -Wmacro-redefined when <linux/minmax.h> has
    already been included.  mali_kbase_debug.h includes mali_malisw.h before
    <linux/module.h>, so Mali defines MIN/MAX first and minmax.h is the
    redefiner -- the warning stayed.  Reverted in favour of deleting the
    private definitions outright, done in the following commit.
    
    Change-Id: I4175c4117412d10415eb88fc6a539266c3649d4d

--- bifrost/r47p0/kernel/drivers/gpu/arm/midgard/mali_malisw.h
+++ bifrost/r47p0/kernel/drivers/gpu/arm/midgard/mali_malisw.h
@@ -36,7 +36,6 @@
  * As a macro it may evaluate its arguments more than once.
  * Refer to MAX macro for more details
  */
-#undef MIN
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 
 /**
@@ -51,7 +50,6 @@
  * to retrieve the min and max of two values, consider using a conditional swap
  * instead.
  */
-#undef MAX
 #define MAX(x, y) ((x) < (y) ? (y) : (x))
 
 /**
